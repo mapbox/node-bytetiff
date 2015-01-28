308f2064a82dbdd113df2ece0118f672af670265
Ryan Clark <ryan.clark.j@gmail.com>
2015-01-27 18:54:18 -0700

    use geo-pixel-stream

--- test/test.js
+++ test/test.js
@@ -7,42 +7,42 @@ var gdal = require('gdal');
 
 
 tape('scale', function(assert) {
-  
+
   var srcpath = path.join(__dirname, 'fixtures', '16bit.tif');
   var dstpath = path.join(__dirname, 'fixtures', 'scaled.tif');
-  
+
   // This file was produced with gdal_translate
   var ctrlpath = path.join(__dirname, 'fixtures', '8bit.tif');
-  
+
   bytetiff.scale(srcpath, dstpath, function(error) {
-    
+
     ctrl = gdal.open(ctrlpath);
     dst = gdal.open(dstpath);
-    
+
     ctrl.bands.forEach(function(band) {
       var bidx = band.id;
-      
+
       var ctrlData = band.pixels.read(0, 0, 100, 100);
-      
+
       var dstband = dst.bands.get(bidx);
       var dstData = dstband.pixels.read(0, 0, 100, 100);
-      
+
       for (var i = 0; i < 100 * 100; i += 1) {
         assert.equal(ctrlData[i], dstData[i]);
       }
-      
+
     });
-    
+
     assert.end();
   });
-  
+
 });
 
 
 tape('teardown', function (test) {
-  
+
   var dstpath = path.join(__dirname, 'fixtures', 'scaled.tif');
   fs.unlink(dstpath);
-  
+
   test.end();
 });
